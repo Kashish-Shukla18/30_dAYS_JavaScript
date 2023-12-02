a78e722fd637c179603c68226be1d2a7e6ffacb7
Kashish Shukla <86675818+Kashish-Shukla18@users.noreply.github.com>
2023-12-02 14:58:55 +0530

    Update script.js

--- To-do List/script.js
+++ To-do List/script.js
@@ -13,6 +13,7 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value="";
+    saveData()
 }
 listContainer.addEventListener("click",function(e){
     if(e.target.tagName==='LI'){
@@ -22,3 +23,9 @@ listContainer.addEventListener("click",function(e){
         e.target.parentElement.remove();
     }
 },false);
+function saveData(){
+localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");}
+showTask();
